4f42d89c311e9b9430110f78d619be00a4810d49
dove209 <53247748+dove209@users.noreply.github.com>
2021-12-14 23:52:06 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-size: 24px;
+`;
+
+const Home: NextPage = () => {
+  return <Container>hello world 안녕</Container>;
+};
+
+export default Home;
