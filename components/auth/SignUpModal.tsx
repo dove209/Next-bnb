ce9152e4824983ce64b5e68f6706e12d8cc3ed5b
youngjin lee <53247748+dove209@users.noreply.github.com>
2021-12-15 18:53:40 +0900

    공용해더 만들기(로그인 모달창 만들기, 스타일 적용x)

---
+++ components/auth/SignUpModal.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 568px;
+    height: 614px;
+    background-color: #fff;
+    z-index: 11;
+`
+
+const SignUpModal: React.FC = () => {
+    return (
+        <Container>
+            Sign UP
+        </Container>
+    )
+}
+
+
+export default SignUpModal;
